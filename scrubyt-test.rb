28038bac148e68ca4b03e78f8b83ea34cfd969e5
jspradlin <jspradlin@gmail.com>
2009-04-11 17:05:12 -0400

    code formatting

--- scrubyt-test.rb
+++ scrubyt-test.rb
@@ -9,13 +9,10 @@ blog_data = Scrubyt::Extractor.define do
   blog_entry '//div[@class="entry-head"]' do
 		blog_url "//h2/a/@href", :format_output => lambda {|x| blog_url + x }
   	blog_title "//h2/a"
-  	blog_author_id "//ul/li/a[@class='blog_usernames_blog']/@href", 
-  		:format_output => lambda {|x| x.split('/')[2]}
-  	blog_author "//ul/li/a[@class='blog_usernames_blog']", 
-  		:format_output => lambda {|x| x.split('&')[0]}
+  	blog_author_id "//ul/li/a[@class='blog_usernames_blog']/@href", :format_output => lambda {|x| x.split('/')[2]}
+  	blog_author "//ul/li/a[@class='blog_usernames_blog']", :format_output => lambda {|x| x.split('&')[0]}
   	blog_date "//span/abbr", :format_output => lambda {|x| x.split(' ')[1]}
-  	number_of_reads "//span[@class='statistics_counter']", 
-  		:format_output => lambda {|x| x.split(' ')[0]}
+  	number_of_reads "//span[@class='statistics_counter']", :format_output => lambda {|x| x.split(' ')[0]}
   end
   
 	next_page "//a[@title='Go to next page']", :limit => 1
